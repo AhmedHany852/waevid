3ae35b3bd6075b51c99538fef12a0b645181db73
NouraMohamed123 <nouramohamed091@gmail.com>
2024-07-14 17:39:33 +0300

    شبب

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -86,8 +86,10 @@ class SettingController extends Controller
         $settings = Setting::pluck('value', 'key')
             ->toArray();
 
-        $image = asset('uploads/settings/' .  $settings['site_logo']);
-        $settings['site_logo'] =    $image;
+        $image = asset('uploads/settings/' .  $settings['site_logo_single']);
+        $settings['site_logo_single'] =    $image;
+        $image2 = asset('uploads/settings/' .  $settings['site_logo_full']);
+        $settings['site_logo_full'] =    $image2;
         return response()->json(['isSuccess' => true, 'data' =>    $settings], 200);
     }
 
