1c9672081afc49c82677459a5e29303b775f8d4b
AhmedHany852 <ahanyebrahim@gmail.com>
2024-07-16 15:59:24 +0300

    h

---
+++ app/Http/Controllers/AppUser/ReviewController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\AppUser;
+
+use App\Http\Controllers\Controller;
+use App\Models\Game;
+use App\Models\Review;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $validatedData = $request->validate([
+            'user_id' => 'required|exists:app_users,id',
+            'reviewable_type' => 'required|string',
+            'reviewable_id' => 'required|integer',
+            'descriptions' => 'nullable|string',
+            'rating' => 'required|numeric|between:1,5',
+        ]);
+        $reviewableType = $request->input('reviewable_type') === 'service' ? Service::class : Game::class;
+        $reviewable = $reviewableType::findOrFail($request->input('reviewable_id'));
+
+        // Create a new review
+        $review = Review::create([
+            'user_id' => $request->user_id,
+            'reviewable_type' => $reviewableType,
+            'reviewable_id' => $reviewable,
+            'descriptions' => $request->descriptions,
+            'rating' => $request->rating,
+        ]);
+
+        // Return a response
+        return response()->json(['successful ' => true, 'data' =>    $review], 200);
+    }
+}
